5ced68eee46345664d79f4719ee88c6d29d2d251
Devin <mcmanusdevin2008@gmail.com>
2024-09-24 12:54:28 -0700

    started start screen

---
+++ TopDownShowcase/Assets/Scripts/StartGame.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField]
+    string levelToLoad = "SampleScene";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        //IF press the button it starts the game
+        if (collision.gameObject.tag == "Start")
+        {
+
+            SceneManager.LoadScene(levelToLoad);
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    public void StartLevel()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
